2bf6f1a6c8990d0ad7c6efbd478065e7e4c2db1d
Magnus-PC\Magnus <magnus.sigvartsen@gmail.com>
2017-02-13 14:09:55 +0100

    Initial Commit

---
+++ BeaverPrototype/Source/BeaverPrototype/BeaverPrototypeGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BeaverPrototypeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEAVERPROTOTYPE_API ABeaverPrototypeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
